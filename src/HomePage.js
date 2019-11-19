458b2796d6a69fe10333d36aa1b1853b948c5d73
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-19 14:41:37 -0800

    updated with video

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import { sizing, spacing } from '@material-ui/system';
 import Typography from '@material-ui/core/Typography';
+import sea from './sea.mp4';
+import CardMedia from '@material-ui/core/CardMedia';
+
 const styles = {
 
   titleContainer:
@@ -19,10 +22,17 @@ class HomePage extends Component {
     const { classes } = this.props;
     return (
       <div >
+        <div>
+          <CardMedia component="video" src={sea} autoPlay>
+
+          </CardMedia>
+        </div>
+
         <div className={classes.titleContainer}>
           <Typography variant='h1'>Home</Typography>
         </div>
-      </div>
+
+      </div >
     )
   }
 
