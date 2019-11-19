0ddc9f25aa1447caee91ccc27b8313089f4d1487
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-18 21:10:44 -0800

    removed css

---
+++ src/HomePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+
+
+class HomePage extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>home
+      </h1>
+      </div>
+    )
+  }
+
+
+}
--- src/NavBar.js
+++ src/NavBar.js
@@ -6,28 +6,23 @@ class NavBar extends Component {
   render() {
     return (
       <div>
-        <div className="nav-bar">
-          <div className='nav-link-box'>
-            <Link to='/'>
-              <h4>Home</h4>
-            </Link>
-          </div>
-          <div className='nav-link-box'>
-            <Link to='/projects'>
-              <h4>Projects</h4>
-            </Link>
-          </div>
-          <div className='nav-link-box'>
-            <Link to='/aboutme'>
-              <h4> About Me</h4>
-            </Link>
-          </div>
-          <div className='nav-link-box'>
-            <Link to='/photo-gallery'>
-              <h4>Photo Gallery</h4>
-            </Link>
-          </div>
-        </div>
+
+        <Link to='/'>
+          <h4>Home</h4>
+        </Link>
+
+        <Link to='/projects'>
+          <h4>Projects</h4>
+        </Link>
+
+        <Link to='/aboutme'>
+          <h4> About Me</h4>
+        </Link>
+
+        <Link to='/photo-gallery'>
+          <h4>Photo Gallery</h4>
+        </Link>
+
       </div >
     );
   }
