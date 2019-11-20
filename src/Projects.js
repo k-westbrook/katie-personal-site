926f2824406e37bf82b4ba94025ce70051ff844d
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-20 10:20:34 -0800

    added projects page component

---
+++ src/Projects.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+class Projects extends Component {
+
+  constructor() {
+    super()
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Projects;
+
+
