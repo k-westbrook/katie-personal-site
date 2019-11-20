663990de69d4bfdea24462bc55979c05da3c2c5a
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-20 15:09:23 -0800

    made contact cards

--- src/Contact.js
+++ src/Contact.js
@@ -9,10 +9,31 @@ import FireplaceTwoToneIcon from '@material-ui/icons/FireplaceTwoTone';
 import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
 import BrightnessLowIcon from '@material-ui/icons/BrightnessLow';
 import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import LinkIcon from '@material-ui/icons/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Button from '@material-ui/core/Button';
 
 
 const styles = {
+  mainTitle:
+  {
+    fontSize: "15vh",
+    textAlign: "center",
+    marginTop: "3vh",
+    color: "#FFFFFF",
+    marginBottom: "8vh",
+    borderBottomStyle: "solid",
+    paddingBottom: "2vh"
 
+  },
+  contactPageContainer: {
+    marginTop: "11vh",
+    padding: '5vh'
+  },
 }
 
 
@@ -27,10 +48,35 @@ class Contact extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
 
-      <div>
+      <div className={classes.contactPageContainer}>
+        <div>
 
+          <Typography variant='h1' className={classes.mainTitle}>Contact Me</Typography>
+        </div>
+        <Card>
+          <CardActionArea>
+            <CardContent>
+              <Typography>Email Me @ kcheriewestbrook@gmail.com</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card>
+          <CardActionArea>
+            <CardContent>
+              <Typography>Connect with me on LinkedIn</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card>
+          <CardActionArea>
+            <CardContent>
+              <Typography>Check out my GitHub Account</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </div>
     )
   }
