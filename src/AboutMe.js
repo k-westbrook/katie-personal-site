2bd8088118b031c7390e6e354877e5659acba418
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-19 20:10:16 -0800

    added parallax

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import { sizing } from '@material-ui/system';
-import { Slide } from '@material-ui/core';
+import keyboard from './keyboard.jpg';
 import Typography from '@material-ui/core/Typography';
 const styles = {
 
   titleContainer:
   {
-    textAlign: "Center",
-    marginTop: "11vh"
+
+    marginTop: "11vh",
+    display: "flex"
+
+  },
+  imageContainer:
+  {
+
+    backgroundImage: `url(${keyboard})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    borderStyle: "solid",
+    height: "100vh",
+    width: "100vw",
+
   }
 };
 
@@ -20,12 +35,17 @@ class AboutMe extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div >
+      <div>
+
+
         <div className={classes.titleContainer}>
-          <Typography variant='h1'>Katie</Typography>
+          <div className={classes.imageContainer}></div>
+
         </div>
         <div>
+          <Typography>About meScroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling. Tip: Try to remove the background-attachment property to remove the scrolling effect.</Typography>
         </div>
+
       </div>
     )
   }
